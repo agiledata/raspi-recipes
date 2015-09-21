6d64f4f7d45646140a030e0e6c82361ae929d823
Rob Collins <rob.collins@agiledata.io>
2015-09-21 15:10:55 +0100

    Example of traffic light operation in student area

--- examples/eg_pi_traffic.py
+++ examples/eg_pi_traffic.py
@@ -3,8 +3,49 @@
 
 from recipes.pi_traffic import *
 
-green
+# Should this be imported?
+red = Light('red')
+amber = Light('amber')
+green = Light('green')
+from time import sleep
+
+def log(n):
+    print(
+        '{n}: red is {red._state}, amber is {amber._state}, green is {green._state}'.format(
+            red=red,
+            amber=amber,
+            green=green,
+            n=n
+        ))
+
+
+#  Here is the essential lesson content.
+log(1)
+red.on()
+log(2)
+
+if True:
+    red.on()
+    log(3)
+    amber.on()
+    log(4)
+    red.off()
+    amber.off()
+    green.on()
+    log(5)
+    green.off()
+    amber.on()
+    log(6)
+    amber.off()
+    red.on()
+    log(7)
+    
+
+
 
 
 if __name__ == '__main__':
     print("Hi")
+
+    # my_pi_traffic = PiTraffic()
+
--- recipes/pi_traffic.py
+++ recipes/pi_traffic.py
@@ -12,7 +12,13 @@ class Light(Hardware):
 
     def __init__(self, color):
         self.color = color
+        self._state = 'off'
 
+    def on(self):
+        self._state = 'on'
+
+    def off(self):
+        self._state = 'off'
 
 # green = Light('green')
 #
